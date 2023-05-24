8936208e5e4afcaa8eac74f08ebf680577109c5e
Cactric <59334627+Cactric@users.noreply.github.com>
2023-05-24 20:44:14 +0100

    Fix: grep the right thing

--- checkoutdatedlibs.sh
+++ checkoutdatedlibs.sh
@@ -8,7 +8,7 @@ main() {
     outdated=0
 
     for pid in $(ps aux | awk '{print $2}' | grep -v "PID"); do
-        if grep -q ' \(outdated\)' "/proc/$pid/maps" 2>/dev/null; then
+        if grep -q ' (deleted)' "/proc/$pid/maps" 2>/dev/null; then
             # TODO: add some edge cases from htop code (see https://github.com/htop-dev/htop/blob/f0a7a78797bd9fd9b8215cc194922d7bc1d6b885/linux/LinuxProcessList.c#L657)
             echo "$pid ($(cat /proc/"$pid"/comm)) is outdated or has outdated libraries"
             outdated=$(( outdated + 1 ))
